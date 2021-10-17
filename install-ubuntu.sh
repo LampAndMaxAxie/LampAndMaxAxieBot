ed906dd75019b09586325f9ba7dfc35856f3901e
Michael Conard <maconard@mtu.edu>
2021-10-17 15:13:18 -0400

    updated install script

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -11,4 +11,6 @@ sudo apt-get install -y libcairo2-dev libgirepository1.0-dev
 
 echo "Installing application requirements"
 pip3 install -r requirements.txt
+pip3 install psutil
+npm install -g electron@1.8.4 orca
 
