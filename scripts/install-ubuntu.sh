47f9493bce8acb62c82f2c3b8ddafc8fb78cfd8d
Michael Conard <maconard@mtu.edu>
2021-11-23 21:07:16 -0500

    updated install script for windows issues with images

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 homedir=/home/ubuntu
-echo "Installing the requirements ... "
 
+# registry update
 echo "Updating source lists"
 sudo apt update 
 
+# python
 echo "Installing python "
 sudo apt install -y python3-pip
 sudo apt-get install -y libcairo2-dev libgirepository1.0-dev
 
-echo "Installing application requirements"
+# python libs
+echo "Installing python libs"
 pip3 install -r requirements.txt
-
-# things for rendering images, only needed for summary/top commands
-npm install -g electron@6.1.4 orca
+pip3 install -r ../requirements.txt
 pip3 install psutil
-sudo apt-get install xvfb
-sudo apt-get install libgtk2.0-0
-sudo apt-get install libgconf-2-4
-sudo apt-get install libxss1
+
+# node stuff
+echo "Installing node stuff"
+sudo apt install nodejs
+sudo apt install npm
+
+# image processing dependencies
+echo "Installing image processing stuff"
+npm install -g electron@6.1.4 orca
+sudo apt install xvfb
+sudo apt install libgtk2.0-0
+sudo apt install libgconf-2-4
+sudo apt install libxss1
+sudo apt install libnss3-dev 
+sudo apt install libgdk-pixbuf2.0-dev 
+sudo apt install libgtk-3-dev 
+sudo apt install libxss-dev
+sudo apt install libasound2
 
