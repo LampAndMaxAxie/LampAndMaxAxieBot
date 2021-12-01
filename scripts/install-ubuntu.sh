55162354967c53fc33e6a0d8351d1413644b28a2
Michael Conard <maconard@mtu.edu>
2021-11-30 23:07:40 -0500

    added some common fixes to the install script

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -11,9 +11,13 @@ sudo apt install -y python3 python3-pip libcairo2-dev libgirepository1.0-dev
 
 # python libs
 echo "Installing python libs"
+python3 -m pip install --upgrade pip
 pip3 install wheel
 pip3 install -r requirements.txt --upgrade
-pip3 install psutil
+python3 -m pip install --upgrade pillow
+pip3 install psutil qrcode aiosqlite 
+
+pip3 install -U kaleido 
 
 # image processing dependencies
 echo "Installing image processing stuff"
